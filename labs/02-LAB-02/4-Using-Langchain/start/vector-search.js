6292687d92d18c8b51a6792a525f6ebb885dc88a
Daniel Fang <qkfang@hotmail.com>
2024-09-10 11:25:43 +0000

    fix

--- labs/02-LAB-02/4-Using-Langchain/start/vector-search.js
+++ labs/02-LAB-02/4-Using-Langchain/start/vector-search.js
@@ -3,7 +3,6 @@ const { MongoClient } = require('mongodb');
 
 // set up the MongoDB client
 const dbClient = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
-var dbname = process.env.MONGODB_Name;
 
 async function main() {
     try {
