89cc2021e578c93bf3224995abc02c8865fb77c3
qkfang@hotmail.com <qkfang@hotmail.com>
2025-01-17 16:58:00 +1100

    one more

--- apps/chatbot/src/pages/seo/Seo.tsx
+++ apps/chatbot/src/pages/seo/Seo.tsx
@@ -1,41 +1,76 @@
 import React, { useState } from "react";
 import { trackPromise } from "react-promise-tracker";
 import { usePromiseTracker } from "react-promise-tracker";
+import { OpenAIClient, AzureKeyCredential, Completions } from '@azure/openai';
+
 
 const Page = () => {
-
     const { promiseInProgress } = usePromiseTracker();
-    const [imageDalleText, setImageDalleText] = useState<string>();
-    const [imageDalleUrl, setImageDalleUrl] = useState<string>("");
+    const [seoUrl, setSeoUrl] = useState<string>();
+    const [seoText, setSeoText] = useState<string>("");
 
-    async function execImageCreateApi() {
-        if (imageDalleText != null) {
+    async function process() {
+        if (seoUrl != null) {
             trackPromise(
-                // dalleApi(imageDalleText);
-            ).then((response) => {
-
-                // console.log(response);
-                setImageDalleUrl(response);
-                // setImageDesc(response);   
+                seoApi(seoUrl)
+            ).then((res) => {
+                setSeoText(res);
             }
-
             )
         }
     }
 
-    const updateText = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setImageDalleText(e.target.value);
-        // console.log('edit->' + task)
-    };
+    async function seoApi(url): Promise<string> {
+        const response = await fetch(url);
+        const html = await response.text();
+        console.log(html);
 
+        var messages =
+            [
+                { "role": "system", "content": "You are a helpful assistant that generate json data based on received xml. output must be json. actually generate the json, don't show me how." },
+                {
+                    "role": "user", "content": [
+                        {
+                            "type": "text",
+                            "text": "Analyse the provided html and summarise it as a json object containing the following fields  : seoKey, seoTitle, seoDescription. \r\n HTML content is here \r\n" + html
+                        }
+                    ]
+                }
+            ];
+        console.log(messages);
+
+        const options = {
+            api_version: "2024-08-01-preview"
+        };
+
+        var openai_url = "https://arg-syd-aiapp1day-openai.openai.azure.com";
+        var openai_key = "e4e18d6e8fc44398b8571c9ba419bf84";
+        const client = new OpenAIClient(
+            openai_url,
+            new AzureKeyCredential(openai_key),
+            options
+        );
+        // ?api-version=2023-12-01-preview
+        const deploymentName = 'gpt4o';
+        const result = await client.getChatCompletions(deploymentName, messages, {
+            maxTokens: 200,
+            temperature: 0.25
+        });
+        return result.choices[0]?.message?.content ?? '';
+    }
+
+    const updateText = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSeoUrl(e.target.value);
+    };
 
     return (
         <div className="pageContainer">
             <h2>SEO</h2>
-            <p></p>
+            <p>Sample product page is here: http://localhost:4000/product.html
+            </p>
             <p>
-                <input type="text" placeholder="describe an image ()" onChange={updateText} />
-                <button onClick={() => execImageCreateApi()}>Start Imagine</button><br />
+                <input type="text" placeholder="(enter url)" onChange={updateText} />
+                <button onClick={() => process()}>Generate</button><br />
                 {
                     (promiseInProgress === true) ?
                         <span>Loading...</span>
@@ -44,7 +79,7 @@ const Page = () => {
                 }
             </p>
             <p>
-                <img height={"550px"} src={imageDalleUrl} />
+                {seoText}
             </p>
         </div>
     );
