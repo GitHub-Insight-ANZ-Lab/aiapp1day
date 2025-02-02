ccdef35f030ab6b1ca52611454ddbab278f3b1c9
raisamyami <samyamis.rai@anko.com>
2025-02-02 12:36:10 +0000

    backend and frontend development done

---
+++ labs/aialchemists/apps/chatbot/src/pages/layout/Layout.tsx
@@ -0,0 +1,49 @@
+import { Outlet, NavLink, Link } from "react-router-dom";
+
+import github from "../../assets/github.svg";
+
+import styles from "./Layout.module.css";
+
+const Layout = () => {
+    return (
+        <div className={styles.layout}>
+            <header className={styles.header} role={"banner"}>
+                <div className={styles.headerContainer}>
+                    <Link to="/" className={styles.headerTitleContainer}>
+                        <h3 className={styles.headerTitle}>LUCAS POS System </h3>
+                    </Link>
+                    <nav>
+                        <ul className={styles.headerNavList}>
+                            {/* <li className={styles.headerNavLeftMargin}>
+                                <a href="/design" >Design</a>
+                            </li> */}
+                            {/* <li>
+                                <a href="/translation" >Translation</a>
+                            </li> */}
+                            {/* <li>
+                                <a href="/vision" >Vision</a>
+                            </li> */}
+                            {/* <li>
+                                <a href="/speech" >Speech</a>
+                            </li> */}
+                            {/* <li>
+                                <a href="/seo" >SEO</a>
+                            </li> */}
+                            {/* <li>
+                                <a href="/search" >Search</a>
+                            </li> */}
+                            {/* <li>
+                                <a href="/automation" >Automation</a>
+                            </li> */}
+                        </ul>
+                    </nav>
+                    {/* <h4 className={styles.headerRightText}>AI App in 1 Day</h4> */}
+                </div>
+            </header>
+
+            <Outlet />
+        </div>
+    );
+};
+
+export default Layout;
---
+++ labs/aialchemists/apps/chatbot/src/pages/seo/Seo.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { trackPromise } from "react-promise-tracker";
+import { usePromiseTracker } from "react-promise-tracker";
+import { OpenAIClient, AzureKeyCredential, Completions } from '@azure/openai';
+
+
+const Page = () => {
+    const { promiseInProgress } = usePromiseTracker();
+    const [seoUrl, setSeoUrl] = useState<string>();
+    const [seoText, setSeoText] = useState<string>("");
+
+    async function process() {
+        if (seoUrl != null) {
+            trackPromise(
+                seoApi(seoUrl)
+            ).then((res) => {
+                setSeoText(res);
+            }
+            )
+        }
+    }
+
+    async function seoApi(url): Promise<string> {
+        // todo
+        return "";
+    }
+
+    const updateText = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSeoUrl(e.target.value);
+    };
+
+    return (
+        <div className="pageContainer">
+            <h2>SEO</h2>
+            <p>Sample product page is here: http://localhost:4000/product.html
+            </p>
+            <p>
+                <input type="text" placeholder="(enter url)" onChange={updateText} />
+                <button onClick={() => process()}>Generate</button><br />
+                {
+                    (promiseInProgress === true) ?
+                        <span>Loading...</span>
+                        :
+                        null
+                }
+            </p>
+            <p>
+                {seoText}
+            </p>
+        </div>
+    );
+};
+
+export default Page;
